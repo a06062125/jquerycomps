bea50173eb7018087f9008a198dd7d8ecb3097cd
suchesqiu <x@btbtd.org>
2013-10-13 17:46:04 +0800

    tmp

--- common.js
+++ common.js
@@ -294,7 +294,7 @@ function pad_char_f( _str, _len, _char ){ return padChar.apply( null, sliceArgs(
  */
 function formatISODate( _date, _split ){
 	_date = _date || new Date(); typeof _split == 'undefined' && ( _split = '-' );
-	return [ _date.getFullYear(), pad_char_f( _date.getMonth() + 1 ), pad_char_f( _date.getDate() ) ].join(_split);
+	return [ _date.getFullYear(), padChar( _date.getMonth() + 1 ), padChar( _date.getDate() ) ].join(_split);
 }
 /**
  * 从 ISODate 字符串解析日期对象
--- lib.js
+++ lib.js
@@ -4,13 +4,13 @@
      * @author  scuehs@btbtd.org 2013-05-23
      * @return  {string} 脚本所在目录的完整路径
      */
-    function script_path_f(){
+    function scriptPath(){
         var _sc = document.getElementsByTagName('script'), _sc = _sc[ _sc.length - 1 ], _path = _sc.getAttribute('src');
         if( /\//.test( _path ) ){ _path = _path.split('/'); _path.pop(); _path = _path.join('/') + '/'; }
         else if( /\\/.test( _path ) ){ _path = _path.split('\\'); _path.pop(); _path = _path.join('\\') + '/'; }
         return _path;
     }
-    var _path = script_path_f();
+    var _path = scriptPath();
     document.write( '<script src="'+_path+'jquery.js" ><\/script>' );
     document.write( '<script src="'+_path+'common.js" ><\/script>' );
     document.write( '<script src="'+_path+'JC.js" ><\/script>' );
