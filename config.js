621ef287dbba3824b54bdc9abd0ee80b6e6ccde4
suchesqiu <x@btbtd.org>
2013-10-13 23:13:44 +0800

    tmp

--- config.js
+++ config.js
@@ -4,19 +4,19 @@
 requirejs.config( {
     baseUrl: '../../../'
     , paths: {
-                'jquery': 'jquery'
-                , 'common': 'common'
-                , 'domReady': 'plugins/domReady'
+        'jquery': 'jquery'
+        , 'common': 'common'
+        , 'domReady': 'plugins/domReady'
 
-                , 'JC.BaseMVC': 'comps/BaseMVC/BaseMVC'
+        , 'JC.BaseMVC': 'comps/BaseMVC/BaseMVC'
 
-                , 'JC.Valid': 'comps/Valid/Valid'
-                , 'JC.Calendar': 'comps/Calendar/Calendar'
+        , 'JC.Valid': 'comps/Valid/Valid'
+        , 'JC.Calendar': 'comps/Calendar/Calendar'
 
-                , 'JC.Form': 'comps/Form/Form'
-                , 'JC.AutoChecked': 'comps/AutoChecked/AutoChecked'
-                , 'JC.AutoSelect': 'comps/AutoSelect/AutoSelect'
-            }
+        , 'JC.Form': 'comps/Form/Form'
+        , 'JC.AutoChecked': 'comps/AutoChecked/AutoChecked'
+        , 'JC.AutoSelect': 'comps/AutoSelect/AutoSelect'
+    }
 });
 
 requirejs( ['jquery', 'common' ] );
